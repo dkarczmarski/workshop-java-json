9e29dc6af5e13067e705fbf489135551769055fe
Dominik Karczmarski <karczmarski@gmail.com>
2021-04-03 12:55:15 +0200

    Refactor JsonObjectWriter

--- src/main/java/my/workshop/json/util/JsonObjectWriter.java
+++ src/main/java/my/workshop/json/util/JsonObjectWriter.java
@@ -3,40 +3,48 @@ package my.workshop.json.util;
 import my.workshop.json.JsonObject;
 import my.workshop.json.JsonValue;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class JsonObjectWriter {
 
-    private static void serialize(StringBuilder sb, JsonValue jv) {
+    private static void write(JsonValue[] a, Writer w) throws IOException {
+        w.append("[");
+        for (int i = 0, n = a.length; i < n; ++i) {
+            if (i > 0) {
+                w.write(",");
+            }
+            write(a[i], w);
+        }
+        w.append("]");
+    }
+
+    private static void write(JsonValue jv, Writer w) throws IOException {
         switch (jv.getType()) {
             case NULL:
-                sb.append("null");
+                w.append("null");
                 break;
 
             case STRING:
-                sb.append("\"").append(jv.getString()).append("\"");
+                w.append("\"").append(jv.getString()).append("\"");
                 break;
 
             case NUMBER:
-                sb.append(jv.getNumberAsString());
+                w.append(jv.getNumberAsString());
                 break;
 
             case BOOLEAN:
-                sb.append(jv.getBoolean());
+                w.append(String.valueOf(jv.getBoolean()));
                 break;
 
             case ARRAY:
-                sb.append("[")
-                        .append(Arrays.stream(jv.getArray())
-                                .map(JsonObjectWriter::toString)
-                                .collect(Collectors.joining(",")))
-                        .append("]");
+                write(jv.getArray(), w);
                 break;
 
             case OBJECT:
-                serialize(sb, jv.getObject());
+                write(jv.getObject(), w);
                 break;
 
             default:
@@ -44,36 +52,34 @@ public class JsonObjectWriter {
         }
     }
 
-    public static String toString(JsonValue jv) {
-        StringBuilder sb = new StringBuilder();
-        serialize(sb, jv);
-        return sb.toString();
-    }
-
-    private static void serialize(StringBuilder sb, JsonObject o) {
-        sb.append("{");
+    public static void write(JsonObject jo, Writer w) throws IOException {
+        w.append("{");
 
         boolean first = true;
-        for (String fieldName : o) {
+        for (String fieldName : jo) {
             if (!first) {
-                sb.append(",");
+                w.append(",");
             } else {
                 first = false;
             }
-            sb.append("\"").append(fieldName).append("\"");
-            sb.append(":");
-            serialize(sb, o.getValue(fieldName));
+            w.append("\"").append(fieldName).append("\"");
+            w.append(":");
+            write(jo.getValue(fieldName), w);
         }
 
-        sb.append("}");
+        w.append("}");
     }
 
     public static String serialize(JsonObject o) {
         Objects.requireNonNull(o);
 
-        StringBuilder sb = new StringBuilder();
-        serialize(sb, o);
-
-        return sb.toString();
+        Writer w = new StringWriter();
+        try {
+            write(o, w);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return w.toString();
     }
+
 }
